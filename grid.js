93d03220ba322e1f4e04f324c944208defe2ec15
EngineerYo <willcrain96@gmail.com>
2019-04-18 12:23:58 -0700

    Updated file

---
+++ grid.js
@@ -0,0 +1,36 @@
+class Grid {
+	constructor(width=50, height=50) {
+		this.width = width-1
+		this.height = height-1
+		this.data = {}
+	}
+
+	set(x, y, val) {
+		if (x > this.width || y > this.height || x < 0 || y < 0) {
+			throw new RangeError('Out of bounds')
+		}
+
+		let sX = x.addLeadingZeros(2)
+		let sY = y.addLeadingZeros(2)
+		let pIndex = `${sX}${sY}`
+
+		this.data[pIndex] = val
+		return this
+	}
+
+	get(x, y) {
+		let sX = x.addLeadingZeros(2)
+		let sY = y.addLeadingZeros(2)
+		let pIndex = `${sX}${sY}`
+
+		return this.data[pIndex]
+	}
+
+	toString() {
+		return JSON.stringify(this.data)
+	}
+
+}
+
+
+module.exports = Grid
