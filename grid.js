7d52bc8d7fac0ef874989b751d277dede71c4907
EngineerYo <willcrain96@gmail.com>
2019-04-18 19:06:53 -0700

    Added class `RoomGrid`

--- grid.js
+++ grid.js
@@ -29,7 +29,34 @@ class Grid {
 	toString() {
 		return JSON.stringify(this.data)
 	}
-
 }
 
+class RoomGrid {
+	constructor(width=50, height=50) {
+		this.width = width-1
+		this.height = height-1
+		this.data = []
+	}
 
+	set(x, y, val) {
+		if (x > this.width || y > this.height || x < 0 || y < 0) {
+			throw new RangeError('Out of bounds')
+		}
+
+		let pIndex = Number(this.width*y + x)
+
+		this.data[pIndex] = val
+		return this
+	}
+
+	get(x, y) {
+		let pIndex = Number(this.width*y + x)
+		return this.data[pIndex]
+	}
+
+	toString() {
+		return JSON.stringify(this.data)
+	}
+}
+
+module.exports = [Grid, RoomGrid]
