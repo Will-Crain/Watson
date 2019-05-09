e5c838915f6400434c1e2bcc88d2217651161990
EngineerYo <willcrain96@gmail.com>
2019-05-09 09:54:55 -0700

    Fixed hostile player check

--- prototype.Room.js
+++ prototype.Room.js
@@ -782,7 +782,7 @@ Room.prototype.sendBiters = function(roomName, amnt=2) {
 
 Room.prototype.defend = function() {
     let hostileCreeps = [...this.find(FIND_HOSTILE_POWER_CREEPS), ...this.find(FIND_HOSTILE_CREEPS)]
-    if (hostileCreeps.length > 0 && !_.any(this.memory.Creeps, s => s.role == 'DEFENSE_MELEE') && _.any(hostileCreeps.owner.username != 'Invader')) {
+    if (hostileCreeps.length > 0 && !_.any(this.memory.Creeps, s => s.role == 'DEFENSE_MELEE') && _.any(hostileCreeps, s => s.owner.username != 'Invader')) {
         this.addCreep('DEFENSE_MELEE', [['DefenseMelee', {}]])
     }
 
