6613ac7d81b29059d15544ce689ee19a0e3db205
EngineerYo <willcrain96@gmail.com>
2019-06-07 21:30:20 -0700

    Created to show sales data

---
+++ Market.js
@@ -0,0 +1,14 @@
+global.showSales = function() {
+    let orders = Game.market.outgoingTransactions
+    let byBuyer = _.groupBy(orders, s => s.recipient ? s.recipient.username : 'SCREEPS')
+    outstr = ''
+    
+    for (let v in byBuyer) {
+        outstr += v + ' (' + _.size(byBuyer[v]) + ' | ' + _.sum(byBuyer[v], s => s.order.price * s.amount).toFixed(2) + ')'
+        for (let p of byBuyer[v]) {
+            outstr += `\n\t<font color=${RES_COLORS[p.resourceType]}>${p.amount}</font>\t(${(p.order.price * p.amount).toFixed(2)})`
+        }
+        outstr += `\n`
+    }
+    console.log(outstr)
+}
