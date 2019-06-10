e0d1fd1023040651d6fc933b92b3f2ca453ded97
EngineerYo <willcrain96@gmail.com>
2019-06-10 10:35:48 -0700

    Cleaned up output

--- Market.js
+++ Market.js
@@ -5,9 +5,9 @@ global.showSales = function() {
     
     for (let v in byBuyer) {
         outstr += v + ' (' + _.size(byBuyer[v]) + ' | ' + _.sum(byBuyer[v], s => s.order.price * s.amount).toFixed(2) + ')'
-        for (let p of byBuyer[v]) {
-            outstr += `\n\t<font color=${RES_COLORS[p.resourceType]}>${p.amount}</font>\t(${(p.order.price * p.amount).toFixed(2)})`
-        }
+        // for (let p of byBuyer[v]) {
+        //     outstr += `\n\t<font color=${RES_COLORS[p.resourceType]}>${p.amount}</font>\t(${(p.order.price * p.amount).toFixed(2)})`
+        // }
         outstr += `\n`
     }
     console.log(outstr)
